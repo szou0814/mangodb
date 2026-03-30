8f9ac1dd5126cbdb9a7a2b628ad49d59c86f5cc3
wetoyo <theduckster1231@gmail.com>
2026-03-30 10:35:13 -0400

    copiued sarah

---
+++ app/__init__.py
@@ -0,0 +1,65 @@
+import sqlite3
+from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
+import os
+import datetime
+import sys
+import random
+import datetime
+import build_user_db as auth
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+DB_FILE="database.db"
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    error_msg = ""
+    if "user_id" in session:
+        return redirect(url_for("home"))
+    if request.method == "POST":
+        username = request.form.get("user_id").strip()
+        password = request.form.get("password").strip()
+
+        if auth.user_exists(username):
+            if auth.login(username, password):
+                session["user_id"] = username
+                return redirect(url_for("home"))
+            else:
+                error_msg = "Password is incorrect."
+        else:
+            error_msg = "User does not exist. Please register."
+    return render_template("login.html", error = error_msg)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    error_msg = ""
+    if "user_id" in session:
+        return redirect(url_for("home"))
+
+    if request.method == "POST":
+        username = request.form.get("user_id").strip()
+        password = request.form.get("password").strip()
+
+        result = auth.register(username, password)
+        if (result == "Registered"):
+            session["user_id"] = username
+            return redirect(url_for("home"))
+        if (result == "Username cannot have special characters except '_'." or result == "Username is already taken." or result == "Username or password cannot be empty."):
+            error_msg = result
+    return render_template("register.html", error = error_msg)
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    if "user_id" in session:
+        session.pop("user_id", None)
+    return redirect(url_for("login"))
+    
+@app.route("/home")
+def home():
+    if "user_id" in session:
+        return render_template("home.html")
+    else:
+        return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
