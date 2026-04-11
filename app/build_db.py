5cd38203caa87c71e88749a77fde8a7b6f705662
Sarah Zou <szou0814@gmail.com>
2026-04-11 02:17:05 -0400

    loaded all datasets

--- app/build_db.py
+++ app/build_db.py
@@ -23,7 +23,7 @@ def create_tbs():
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     c.execute (f"CREATE TABLE IF NOT EXISTS USER(user_id TEXT PRIMARY KEY, password TEXT)")
-    c.execute (f"CREATE TABLE IF NOT EXISTS STATES(state_id TEXT PRIMARY KEY, vulnerability_index INTEGER, population INTEGER, population_density FLOAT)")
+    c.execute (f"CREATE TABLE IF NOT EXISTS STATES(state_id TEXT PRIMARY KEY, vulnerability_index FLOAT, population INTEGER, population_density FLOAT)")
     c.execute (f"CREATE TABLE IF NOT EXISTS stringency(stringency_id INTEGER PRIMARY KEY AUTOINCREMENT, state_id TEXT, date DATE, stringency_index FLOAT, FOREIGN KEY (state_id) REFERENCES states(state_id))")
     c.execute (f"CREATE TABLE IF NOT EXISTS covid_stats(stats_id INTEGER PRIMARY KEY AUTOINCREMENT, state_id TEXT, date DATE, infected INTEGER, dead INTEGER, FOREIGN KEY (state_id) REFERENCES states(state_id))")
     c.execute (f"CREATE TABLE IF NOT EXISTS runs(run_id INTEGER PRIMARY KEY AUTOINCREMENT, state_id TEXT, total_infected INTEGER, total_dead INTEGER, FOREIGN KEY (state_id) REFERENCES states(state_id))")
@@ -34,16 +34,81 @@ def create_tbs():
 def load_states():
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-
-    with open("data/state_trends") as file:
+    with open("data/SVI2020_US_COUNTY.csv", newline="") as f:
         reader = csv.DictReader(f)
-
+        states_totpop = {}
+        states_totarea = {}
+        states_totsvi = {}
+        states_numcounties = {}
         for row in reader:
-            population = int(row["population"])
-            population_density = float(row["pop_density"])
+            state_id = row["STATE"].strip() #WESLEY: all the datsets have the full state name, so change logic.py to use full name instead of abbreviations 
+            try:
+                vulnerability_index = float(row["RPL_THEMES"]) #rpl gives the percentile of vulnerability, spl gives the raw score but i think better to use rpl so we dont have to do additional math to normalize
+                population = int(row["E_TOTPOP"]) #E means estimate, the other field is M which is the margin of error 
+                area = float(row["AREA_SQMI"])
+            except (ValueError, TypeError):
+                continue 
+            if population <= 0 or area <= 0:
+                continue
+            if state not in states_totpop:
+                states_totpop[state_id] = 0
+                states_totarea[state_id] = 0.0
+                states_totsvi[state_id] = 0.0
+                states_numcounties[state_id] = 0
+            states_totpop[state_id] += population
+            states_totarea[state_id] += area
+            states_totsvi[state_id] += vulnerability_index
+            states_numcounties[state_id] += 1
+        for state in states_totpop:
+            avg_vulnerability_index = states_totsvi[state] / states_numcounties[state]
+            population_density = states_totpop[state] / states_totarea[state]
+            c.execute("INSERT INTO STATES (state_id, vulnerability_index, population, population_density) VALUES (?, ?, ?, ?)", (state_id, avg_vulnerability_index, states_totpop[state], population_density))
 
-    c.execute(f"INSERT s)
+    db.commit()
+    db.close()
 
+def format_date(date):
+    #oxford stringency dataset date format: 
+    #YYYY MM DD 
+    return f"{date[:4]}-{date[4:6]}-{date[6:]}"
+
+def load_stringency():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    with open("data/OxCGRT_US_latest.csv", newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            state_id = row["RegionName"]
+            if not state_id:
+                continue
+            try:
+                date = format_date(row["Date"])
+                stringency_index = float(row["StringencyIndex"])
+            except (ValueError, TypeError):
+                continue
+            c.execute("INSERT INTO stringency(state_id, date, stringency_index) VALUES (?, ?, ?)", (state_id, date, stringency_index))
+
+    db.commit()
+    db.close()
+
+def load_covid():
+    #    c.execute (f"CREATE TABLE IF NOT EXISTS covid_stats(stats_id INTEGER PRIMARY KEY AUTOINCREMENT, state_id TEXT, date DATE, infected INTEGER, dead INTEGER, 
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    with open("data/us-states.csv", newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            state_id = row["state"]
+            try:
+                date = row["date"]
+                infected = int(row["cases"])
+                dead = int(row["deaths"])
+            except (ValueError, TypeError):
+                continue
+            c.execute("INSERT INTO covid_stats (state_id, date, infected, dead) VALUES (?, ?, ?, ?)", (state_id, date, infected, dead))
+
+    db.commit()
+    db.close()
 
 #USER#############################################################################################
 #checks if username already in db
