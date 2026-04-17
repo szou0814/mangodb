2106becac524a237bdf8242e9cbda4fefc8622a7
wetoyo <theduckster1231@gmail.com>
2026-04-17 01:43:53 -0400

    jia you

--- app/logic.py
+++ app/logic.py
@@ -3,6 +3,7 @@ import urllib.request
 from datetime import datetime, timedelta
 from build_db import get_statestats, get_initstringency
 from random import random
+
 STATES = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY",
           "LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND",
           "OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
@@ -11,12 +12,11 @@ STATE_NAMES = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","
     "Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon",
     "Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia",
     "Wisconsin","Wyoming"]
-## we gonna ignore alaska and hawaii cuz cmon bro
+
 def convert(state: str):
-    # convert between the abrv and the name
     return STATE_NAMES[STATES.index(state)]
+
 def get_adjacency():
-    # URL to the adjacency map
     url = "https://gist.githubusercontent.com/longbai/44f446bc907ada728948e4c15aca252e/raw"
     with urllib.request.urlopen(url) as response:
         content = response.read().decode('utf-8')
@@ -30,30 +30,29 @@ def get_adjacency():
             adj_map[state] = neighbors
     return adj_map
 
-
-
-
-
-
-def run_simulation(start_state: str, adj_map):
-    # dataframe with the states as rows and cols being datetimes
-    valid_states = [s for s in STATES if s in adj_map]
-    pop = pd.DataFrame(index=valid_states)
+def run_simulation(start_state: str, adj_map, covid_models=None):
+    pop = pd.DataFrame(index=STATES)
 
     start_date = datetime(2020,1,20)
     pop[f"{start_date}_infected"] = 0.0
+    pop[f"{start_date}_resistant"] = 0.0
 
     def get_initial_population(state: str):
         return get_statestats(convert(state))[1]
 
     vulnerability_index = []
     population_density = []
+    stringency_index = []
+    
     def load_state_info(state:str):
         nonlocal vulnerability_index
         nonlocal population_density
+        nonlocal stringency_index
         state_info = [get_statestats(state) for state in STATE_NAMES if state]
+        str_info = [get_initstringency(state) for state in STATE_NAMES if state]
         vulnerability_index = [s[0] for s in state_info]
         population_density = [s[2] for s in state_info]
+        stringency_index = [s[0] if s else 0.0 for s in str_info]
 
     pop[f"{start_date}_population"] = [get_initial_population(state) for state in pop.index]
 
@@ -61,84 +60,106 @@ def run_simulation(start_state: str, adj_map):
         if state not in pop.index:
             print("Error: State name incorrect. Maybe you forgot capitalization?")
             return
-        pop.loc[state, f"{date}_infected"] = 1.0  # Start with 1 person infected
+        pop.loc[state, f"{date}_infected"] = 1.0
         load_state_info(state)
 
-    # takes the state to select adn then calls select state
     select_state(start_state, start_date)
 
     def infect(state: str, date):
         idx = STATES.index(state)
-        # ill updatye this later with the regression
-        return pop.loc[state, f"{date}_infected"] * (1 + ((vulnerability_index[idx]*10)/population_density[idx])) + population_density[idx]
+        curr_inf = pop.loc[state, f"{date}_infected"]
+        curr_pop = pop.loc[state, f"{date}_population"]
+        curr_res = pop.loc[state, f"{date}_resistant"]
+        eff_pop = max(0, curr_pop - curr_res)
+        
+        if covid_models is not None:
+             new_infections = covid_models.predict_new_infections(eff_pop, population_density[idx], curr_inf, vulnerability_index[idx], stringency_index[idx])
+             return curr_inf + new_infections
+        return curr_inf * (1 + ((vulnerability_index[idx]*10)/population_density[idx])) + population_density[idx]
 
     def spread(state: str, date, adjacency_map, inf_state):
-        # Calculates infections spreading
         spread_amount = 0.0
+        pop_state = pop.loc[state, f"{date}_population"]
+        res_state = pop.loc[state, f"{date}_resistant"]
+        eff_pop_state = max(0, pop_state - res_state)
+        susceptible = max(0, eff_pop_state - inf_state)
+
         for neighbor in adjacency_map.get(state, []):
-            if neighbor in pop.index:
-                inf_neighbor = pop.loc[neighbor, f"{date}_infected"]
-                if inf_neighbor == 0:
-                    continue
+            if neighbor not in pop.index:
+                continue
 
-                pop_neighbor = pop.loc[neighbor, f"{date}_population"]
-                pop_state = pop.loc[state, f"{date}_population"]
+            inf_neighbor = pop.loc[neighbor, f"{date}_infected"]
+            if inf_neighbor <= 0:
+                continue
 
-                uninf_neighbor = max(0, pop_neighbor - inf_neighbor)
-                uninf_state = max(0, pop_state - inf_state)
+            pop_neighbor = pop.loc[neighbor, f"{date}_population"]
+            res_neighbor = pop.loc[neighbor, f"{date}_resistant"]
+            eff_pop_neighbor = max(0, pop_neighbor - res_neighbor)
+            susceptible_neighbor = max(0, eff_pop_neighbor - inf_neighbor)
 
-                total_uninfected = uninf_state + uninf_neighbor
-                if total_uninfected > 0:
+            total_susceptible = susceptible + susceptible_neighbor
+            if total_susceptible == 0:
+                continue
 
-                    # update this logic later with stringency and whatever
-                    chance = inf_neighbor / total_uninfected
-                    # spread like 1000 people * chance
-                    spread_amount += round(chance * 2000)
+            combined_eff_pop = eff_pop_state + eff_pop_neighbor
+            idx = STATES.index(state)
 
-        return spread_amount
+            if covid_models is not None:
+                total_new = covid_models.predict_new_infections(combined_eff_pop, population_density[idx], inf_neighbor, vulnerability_index[idx], stringency_index[idx])
+            else:
+                total_new = inf_neighbor * ((vulnerability_index[idx]*10)/population_density[idx]) + population_density[idx]
 
-    def get_death_rate(state: str):
-        # update this later with whaetver
-        # make teh death rate a function of stringency r smtjh
-        return (random()* 0.023 + 0.0015)
+            share_to_state = susceptible / total_susceptible
+            spread_amount += total_new * share_to_state
+        return min(susceptible, spread_amount * 0.01)
+    def get_deaths_for_state(state: str, curr_pop, past_infected):
+        idx = STATES.index(state)
+        if covid_models is not None:
+            return covid_models.predict_deaths(curr_pop, population_density[idx], past_infected, vulnerability_index[idx], stringency_index[idx])
+        return past_infected * (random() * 0.023 + 0.0015)
 
-    def calculate_deaths(state: str, current_date):
+    def calculate_deaths_and_resistance(state: str, current_date):
         week_ago = current_date - timedelta(days=7)
         curr_pop = pop.loc[state, f"{current_date}_population"]
+        curr_res = pop.loc[state, f"{current_date}_resistant"]
 
         col_week_ago = f"{week_ago}_infected"
         if col_week_ago in pop.columns:
             past_infected = pop.loc[state, col_week_ago]
-            deaths = past_infected * get_death_rate(state)
-            return max(0, curr_pop - deaths)
-        return curr_pop
+            deaths = get_deaths_for_state(state, curr_pop, past_infected)
+                
+            survivors = max(0, past_infected - deaths)
+            new_pop = max(0, curr_pop - deaths)
+            new_res = min(new_pop, round(curr_res + survivors / 2))
+            return new_pop, new_res, survivors / 2
+            
+        return curr_pop, curr_res, 0
 
     def step(current_date, next_date, adjacency_map):
         nonlocal pop
         new_infected_col = []
         new_population_col = []
+        new_resistant_col = []
 
         for state in pop.index:
-
-            # Update population based on deaths
-            new_pop = round(calculate_deaths(state, current_date))
+            new_pop, new_res, resolved_infections = calculate_deaths_and_resistance(state, current_date)
             new_population_col.append(new_pop)
+            new_resistant_col.append(new_res)
 
-            # Update infected
             base_inf = infect(state, current_date) if pop.loc[state, f"{current_date}_infected"] > 0 else 0
             spread_inf = spread(state, current_date, adjacency_map, base_inf)
 
-            total_inf = round(base_inf + spread_inf)
-            new_infected_col.append((min(total_inf, new_pop)))
+            total_inf = round(base_inf + spread_inf - resolved_infections)
+            new_infected_col.append(max(0, min(total_inf, new_pop - new_res)))
 
         new_cols_df = pd.DataFrame({
             f"{next_date}_infected": new_infected_col,
+            f"{next_date}_resistant": new_resistant_col,
             f"{next_date}_population": new_population_col
         }, index=pop.index)
 
         pop = pd.concat([pop, new_cols_df], axis=1)
 
-    # loop params, terminates if entire us is infected
     max_iters = 156
     curr_date = start_date
 
@@ -148,18 +169,25 @@ def run_simulation(start_state: str, adj_map):
         step(curr_date, next_date, adj_map)
 
         total_infected = pop[f"{next_date}_infected"].sum()
+        total_resistant = pop[f"{next_date}_resistant"].sum()
         total_population = pop[f"{next_date}_population"].sum()
 
-        if total_population == 0 or total_infected >= total_population * 0.999:
-            print(f"THEY ALL DIED womp womp womp or maybe they all js infected so sorry")
+        if total_population == 0 or (total_infected + total_resistant) >= total_population * 0.999:
+            print("THEY ALL DIED womp womp womp or maybe they all js infected so sorry")
             break
 
         curr_date = next_date
     return pop
 
 if __name__ == "__main__":
+    from model import get_models
+    import os
+    
+    # i need ts cuz its ugly
+    pd.options.display.float_format = '{:.0f}'.format
+    
+    models = get_models()  
     adj_map = get_adjacency()
-    final_df = run_simulation("NY", adj_map)
-    #print(final_df)
-    print(final_df.iloc[:, [-4, -3, -2, -1]])
-#print(get_statestats(convert("NY"))[1])
+    
+    final_df = run_simulation("NY", adj_map, covid_models=models)
+    print(final_df.iloc[:, -6:])
---
+++ app/model.py
@@ -0,0 +1,156 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import os
+import numpy as np
+
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(5, 16)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(16, 1)
+        
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+
+class CovidModels:
+    def __init__(self):
+        self.inf_model = None
+        self.death_model = None
+        self.inf_stats = None
+        self.death_stats = None
+
+    def train(self, data_dir="data", epochs=50):
+
+        # parse svi data
+        svi_df = pd.read_csv(os.path.join(data_dir, "SVI_2020_US_county.csv"))
+        state_stats = svi_df.groupby("STATE").apply(
+            lambda x: pd.Series({
+                "vulnerability": x["RPL_THEMES"][x["RPL_THEMES"]>=0].mean()
+            })
+        ).reset_index()
+        state_stats.rename(columns={"STATE": "state"}, inplace=True)
+        
+        # parse state_trends
+        trends_df = pd.read_csv(os.path.join(data_dir, "state_trends.csv"))
+        pop_map = dict(zip(trends_df["state"], trends_df["population"]))
+        density_map = dict(zip(trends_df["state"], trends_df["pop_density"]))
+        
+        state_stats["population"] = state_stats["state"].map(pop_map)
+        state_stats["density"] = state_stats["state"].map(density_map)
+
+        # parse covid data
+        covid_df = pd.read_csv(os.path.join(data_dir, "us-states.csv"))
+        covid_df["date"] = pd.to_datetime(covid_df["date"])
+        covid_df = covid_df.groupby(["state", pd.Grouper(key="date", freq="W")]).max().reset_index()
+        covid_df.sort_values(["state", "date"], inplace=True)
+
+        covid_df["new_cases"] = covid_df.groupby("state")["cases"].diff().fillna(0)
+        covid_df["new_deaths"] = covid_df.groupby("state")["deaths"].diff().fillna(0)
+
+        covid_df["current_infected"] = covid_df.groupby("state")["new_cases"].shift(1).fillna(0) + covid_df.groupby("state")["new_cases"].shift(2).fillna(0)
+        covid_df["target_new_cases"] = covid_df.groupby("state")["new_cases"].shift(-1).fillna(0)
+        covid_df["target_new_deaths"] = covid_df.groupby("state")["new_deaths"].shift(-1).fillna(0)
+
+        # parse stringency data
+        ox_df = pd.read_csv(os.path.join(data_dir, "OxCGRT_US_latest.csv"), dtype={"Date": str})
+        ox_df = ox_df.dropna(subset=["RegionName"])
+        ox_df["date"] = pd.to_datetime(ox_df["Date"])
+        ox_weekly = ox_df.groupby(["RegionName", pd.Grouper(key="date", freq="W")]).agg({
+            "StringencyIndex": "mean"
+        }).reset_index()
+        ox_weekly.rename(columns={"RegionName": "state", "StringencyIndex": "stringency"}, inplace=True)
+
+        df = pd.merge(covid_df, state_stats, on="state", how="inner")
+        df = pd.merge(df, ox_weekly, on=["state", "date"], how="inner")
+        df = df.dropna()
+
+        X = df[["population", "density", "current_infected", "vulnerability", "stringency"]].values.astype(np.float32)
+        y_inf = df[["target_new_cases"]].values.astype(np.float32)
+        y_death = df[["target_new_deaths"]].values.astype(np.float32)
+
+        def _train_internal(X_data, y_data):
+            model = SimpleModel()
+            criterion = nn.MSELoss()
+            optimizer = optim.Adam(model.parameters(), lr=0.01)
+            
+            X_mean = X_data.mean(axis=0)
+            X_std = X_data.std(axis=0) + 1e-8
+            X_norm = (X_data - X_mean) / X_std
+            
+            y_mean = y_data.mean(axis=0)
+            y_std = y_data.std(axis=0) + 1e-8
+            y_norm = (y_data - y_mean) / y_std
+
+            tensor_X = torch.tensor(X_norm.tolist())
+            tensor_y = torch.tensor(y_norm.tolist())
+            
+            for epoch in range(epochs):
+                optimizer.zero_grad()
+                outputs = model(tensor_X)
+                loss = criterion(outputs, tensor_y)
+                loss.backward()
+                optimizer.step()
+                
+            return model, (X_mean, X_std, y_mean, y_std)
+
+        self.inf_model, self.inf_stats = _train_internal(X, y_inf)
+        self.death_model, self.death_stats = _train_internal(X, y_death)
+
+    def load(self, model_path="models.pth"):
+        data = torch.load(model_path)
+        self.inf_model = SimpleModel()
+        self.inf_model.load_state_dict(data['inf_model'])
+        self.inf_model.eval()
+        self.inf_stats = data['inf_stats']
+        
+        self.death_model = SimpleModel()
+        self.death_model.load_state_dict(data['death_model'])
+        self.death_model.eval()
+        self.death_stats = data['death_stats']
+
+    def save(self, model_path="models.pth"):
+        torch.save({
+            'inf_model': self.inf_model.state_dict(),
+            'inf_stats': self.inf_stats,
+            'death_model': self.death_model.state_dict(),
+            'death_stats': self.death_stats
+        }, model_path)
+
+    def _predict(self, model, stats, population, density, current_infected, vulnerability, stringency):
+        x_mean, x_std, y_mean, y_std = stats
+        x_input = [float(population), float(density), float(current_infected), float(vulnerability), float(stringency)]
+        x_norm = [(x_input[i] - x_mean[i]) / x_std[i] for i in range(5)]
+        tensor_x = torch.tensor([x_norm], dtype=torch.float32)
+        
+        with torch.no_grad():
+            output_norm = model(tensor_x).item()
+            
+        output = output_norm * y_std[0] + y_mean[0]
+        return max(0, output)
+
+    def predict_new_infections(self, population, density, current_infected, vulnerability, stringency):
+        return self._predict(self.inf_model, self.inf_stats, population, density, current_infected, vulnerability, stringency)
+
+    def predict_deaths(self, population, density, current_infected, vulnerability, stringency):
+        return self._predict(self.death_model, self.death_stats, population, density, current_infected, vulnerability, stringency)
+
+def get_models():
+    models = CovidModels()
+    model_path = os.path.join(os.path.dirname(__file__), "models.pth")
+    data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
+    
+    if os.path.exists(model_path):
+        models.load(model_path)
+    else:
+        print(f"gyat to train first")
+        models.train(data_dir=data_dir)
+        models.save(model_path)
+        print(f"finished and saved to {model_path}")
+        
+    return models
+
+if __name__ == "__main__":
+    get_models()
